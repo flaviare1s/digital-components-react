f084236c28febbfaf91d8579d09cae41b2fc7271
Flávia Reis <flaviarster@gmail.com>
2024-01-19 09:55:25 -0300

    Create components

---
+++ src/components/Component1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Component1 = () => {
+  return (
+    <div>
+        <h2>Componente 1</h2>
+        <button>Clique aqui!</button>
+    </div>
+  )
+}
---
+++ src/components/Component2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Component2 = () => {
+  return (
+    <div>
+        <h3>Componente 2</h3>
+    </div>
+  )
+}
+
