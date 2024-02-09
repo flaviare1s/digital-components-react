89da0780d081c05c4cf14722f2a3aecf493f5400
Flávia Reis <flaviarster@gmail.com>
2024-02-09 09:35:58 -0300

    Ajeitei estilos

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import {
-  createHashRouter,
+  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
 import { Product } from './pages/Product.jsx';
-import { ProductsContext } from './pages/providers/products.jsx';
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <div><App/></div>,
@@ -22,8 +21,6 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProductsContext.Provider>
-      <RouterProvider router={router} />
-    </ProductsContext.Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
